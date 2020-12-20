b7da3c6d01ea6b495f5fceb1cd1617ab74c42970
Juan Carlos Sanchez Ruiz de Chavec <RXWP5657@gmail.com>
2020-12-20 01:09:37 -0600

    Adding unit testing skeleton and hashable interface and entity class

---
+++ tests/sparseSet.test.ts
@@ -0,0 +1,23 @@
+describe("Sparse Set testing", () => {
+
+    test(`it should only use classes that implement 
+          the Hashable interface as sparse-set key`, () => {
+        
+    })
+
+    test("it should associate an element E with key K", () => {
+
+    })
+
+    test("it should retrieve the element associated to a key K", () => {
+
+    })
+
+    test("it should delete an element E associated to a key K", () => {
+
+    })
+
+    test("it should retrieve all elements stored on the sparse-set", () => {
+
+    })
+})
